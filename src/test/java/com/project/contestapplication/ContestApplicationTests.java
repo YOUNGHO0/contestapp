c7f923b68ce9064a6cdad25f7b41cbfa0a02d072
YOUNGHO0 <31pqopq13@gmail.com>
2024-02-23 14:24:40 +0900

    Feat: 프로젝트 시작

---
+++ src/test/java/com/project/contestapplication/ContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.contestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
